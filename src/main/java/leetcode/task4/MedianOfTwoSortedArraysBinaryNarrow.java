851b35658ab9ca9abae4d1fe52b61ece02d273a9
rafalzonk <rafalzonk@gmail.com>
2024-01-19 22:26:48 +0100

    Resolved and accepted solution for Task 4

--- src/main/java/leetcode/task4/MedianOfTwoSortedArraysBinaryNarrow.java
+++ src/main/java/leetcode/task4/MedianOfTwoSortedArraysBinaryNarrow.java
@@ -51,7 +51,7 @@ public class MedianOfTwoSortedArraysBinaryNarrow implements MedianOfTwoSortedArr
             }
         }
 
-        // two single
+        // two singles
         if (right1 - left1 == 0 && right2 - left2 == 0) {
             return (nums1[left1] + nums2[left2]) / 2.0;
         }
@@ -73,28 +73,82 @@ public class MedianOfTwoSortedArraysBinaryNarrow implements MedianOfTwoSortedArr
             }
 
             // one single, one odd
-            if ((right - left) % 2 == 0) {
-                double oddMedian = median(left, right, nums);
-                if (oddMedian == single) {
-                    return single;
-                }
-                if (oddMedian > single) {
-                    return median(left, right - 1, nums);
-                }
-                return median(left + 1, right, nums);
+            return resolveOneSingleElement(right, left, nums, single);
+        } else { //at least one double
+            int first, second;
+            int left, right;
+            int[] nums, duble;
+            if (right1 - left1 == 1) {
+                first = left1;
+                second = right1;
+                left = left2;
+                right = right2;
+                nums = nums2;
+                duble = nums1;
+            } else {
+                first = left2;
+                second = right2;
+                left = left1;
+                right = right1;
+                nums = nums1;
+                duble = nums2;
             }
-            //one single, one even
-            else {
-                double evenMedianIndex = medianIndex(left, right);
-                left = nums[(int) evenMedianIndex];
-                right = nums[(int) (evenMedianIndex + 1)];
-                if (single > right)
-                    return right;
-                return max(single, left);
+
+            while (right - left >= 2 && second > first) {
+                if (duble[first] > nums[left])
+                    left++;
+                else
+                    first++;
+
+                if (duble[second] < nums[right])
+                    right--;
+                else
+                    second--;
             }
-        } else {
-            //two doubles
-            return (max(nums1[left1], nums2[left2]) + min(nums1[right1], nums2[right2])) / 2.0;
+
+            if (second == first) {
+                return resolveOneSingleElement(left, right, nums, duble[first]);
+            }
+            if (right == left) {
+                return resolveOneSingleElement(first, second, duble, nums[left]);
+            }
+            if (second < first) { //median within the longer array
+                return median(left, right, nums);
+            } else {
+                //two double arrays
+                return (max(nums[left], duble[first]) + min(nums[right], duble[second])) / 2.0;
+            }
+
+        }
+    }
+
+    private double resolveOneSingleElement(int left, int right, int[] nums, int single) {
+        if ((right - left) % 2 == 0) {
+            double oddMedian = median(left, right, nums);
+            int medianIndex = (right + left) / 2;
+            if (oddMedian == single) {
+                return single;
+            }
+            if (oddMedian > single) {
+                if (nums[medianIndex - 1] < single)
+                    return (oddMedian + single) / 2.0;
+                else
+                    return (oddMedian + nums[medianIndex - 1]) / 2.0;
+            }
+            //oddMedian < single
+            if (nums[medianIndex + 1] > single)
+                return (oddMedian + single) / 2.0;
+            else
+                return (oddMedian + nums[medianIndex + 1]) / 2.0;
+        }
+        //one single, one even
+        else {
+            double evenMedianIndex = medianIndex(left, right);
+            left = nums[(int) evenMedianIndex];
+            right = nums[(int) (evenMedianIndex + 1)];
+            if (single > right)
+                return right;
+            return max(single, left);
         }
     }
 
@@ -110,8 +164,7 @@ public class MedianOfTwoSortedArraysBinaryNarrow implements MedianOfTwoSortedArr
     }
 
     private static boolean arraysNarrowable(int right1, int left1, int right2, int left2) {
-        return right2 - left2 >= 1 &&
-                right1 - left1 >= 1 &&
-                right2 + right1 - left2 - left1 > 2;
+        return right2 - left2 > 1 &&
+                right1 - left1 > 1;
     }
 }
