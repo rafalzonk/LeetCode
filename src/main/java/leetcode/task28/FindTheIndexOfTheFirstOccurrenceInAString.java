2a24dc9e5f798463220498aa121fbc33874d9ab4
rafalzonk <rafalzonk@gmail.com>
2024-01-26 09:30:12 +0100

    Find the Index of the First Occurrence in a String

---
+++ src/main/java/leetcode/task28/FindTheIndexOfTheFirstOccurrenceInAString.java
@@ -0,0 +1,7 @@
+package leetcode.task28;
+
+public class FindTheIndexOfTheFirstOccurrenceInAString {
+    public int strStr(String haystack, String needle) {
+        return haystack.indexOf(needle);
+    }
+}
