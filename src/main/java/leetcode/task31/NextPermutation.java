9d80355a78cb2f96cfe7599b316b0603ead401cf
rafalzonk <rafalzonk@gmail.com>
2024-01-28 20:44:25 +0100

    Next Permutation with better performance

--- src/main/java/leetcode/task31/NextPermutation.java
+++ src/main/java/leetcode/task31/NextPermutation.java
@@ -1,9 +1,5 @@
 package leetcode.task31;
 
-import java.util.HashMap;
-import java.util.Map;
-import java.util.TreeMap;
-
 public class NextPermutation {
     public void nextPermutation(int[] nums) {
         if (nums.length == 1)
@@ -14,19 +10,16 @@ public class NextPermutation {
             return;
         }
 
-        Map<Integer, Integer> counting = new TreeMap<>();
         int max = nums[nums.length - 1];
         int i = nums.length - 1;
         while (i >= 0 && nums[i] >= max) {
             if (nums[i] > max) {
                 max = nums[i];
             }
-            int count = counting.getOrDefault(nums[i], 0);
-            counting.put(nums[i], count + 1);
             i--;
         }
         if (i < 0) {
-            revertArray(nums);
+            revertArray(nums, 0);
             return;
         }
 
@@ -34,31 +27,16 @@ public class NextPermutation {
         int min = nums[j];
         int minIndex = j;
         while (j < nums.length) {
-            if (nums[j] > nums[i] && nums[j] < min) {
+            if (nums[j] > nums[i] && nums[j] <= min) {
                 min = nums[j];
                 minIndex = j;
             }
             j++;
         }
 
-        counting.put(nums[i], counting.getOrDefault(nums[i], 0) + 1);
         swap(nums, i, minIndex);
-        int count = counting.get(nums[i]);
-        if (count == 1) {
-            counting.remove(nums[i]);
-        } else {
-            counting.put(nums[i], count - 1);
-        }
+            revertArray(nums, i + 1);
 
-        i++;
-        for (int k : counting.keySet()) {
-            count = counting.get(k);
-            while (count > 0) {
-                nums[i] = k;
-                i++;
-                count--;
-            }
-        }
     }
 
     private static void swap(int[] nums, int a, int b) {
@@ -67,9 +45,9 @@ public class NextPermutation {
         nums[b] = tmp;
     }
 
-    private void revertArray(int[] nums) {
-        for (int i = 0; i < nums.length / 2; i++) {
-            swap(nums, i, nums.length - i - 1);
+    private void revertArray(int[] nums, int from) {
+        for (int i = 0; i + from < (nums.length + from) / 2; i++) {
+            swap(nums, i + from, nums.length - i - 1);
         }
     }
 }
