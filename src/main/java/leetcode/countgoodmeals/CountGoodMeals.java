2344f35829844d5c1afb4641501e870586d1c2bf
rafalzonk <rafalzonk@gmail.com>
2026-02-24 07:42:50 +0100

    LeetCode-1711: Good Meals - removed unnecessary sorting

--- src/main/java/leetcode/countgoodmeals/CountGoodMeals.java
+++ src/main/java/leetcode/countgoodmeals/CountGoodMeals.java
@@ -1,12 +1,11 @@
 package leetcode.countgoodmeals;
 
-import static java.util.Arrays.sort;
+import java.util.Arrays;
 
 public class CountGoodMeals {
     static int[] powersOfTwo = {1, 2, 4, 8, 16, 32, 64, 128, 256, 512, 1024, 2048, 4096, 8192, 16384, 32768, 65536, 131072, 262144, 524288, 1048576, 2097152, 4194304};
 
     public int countPairs(int[] deliciousness) {
-        sort(deliciousness);
         int[] convertedDeliciousness = convertDeliciousness(deliciousness);
 
         long result = 0;
@@ -39,10 +38,13 @@ public class CountGoodMeals {
     }
 
     private int[] convertDeliciousness(int[] deliciousness) {
-        int[] result = new int[deliciousness[deliciousness.length - 1] + 1];
-        for (int j : deliciousness) {
-            result[j]++;
+        final int max = Arrays.stream(deliciousness).max().getAsInt();
+        int[] result = new int[max + 1];
+
+        for (int i : deliciousness) {
+            result[i]++;
         }
+
         return result;
     }
 }
