7b3ab53d9f414c22c4224f5813f4a1cfc7848c60
rafalzonk <rafalzonk@gmail.com>
2026-07-16 12:57:41 +0200

    task53: Maximum Subarray

---
+++ src/main/java/leetcode/maximum_subarray/MaximumSubarray.java
@@ -0,0 +1,18 @@
+package leetcode.maximum_subarray;
+
+public class MaximumSubarray {
+    public int maxSubArray(int[] nums) {
+
+        int maxSum = nums[0];
+        int sum = 0;
+        for (int i = 0; i < nums.length; i++) {
+            sum += nums[i];
+            if (sum > maxSum)
+                maxSum = sum;
+            if (sum < 0)
+                sum = 0;
+        }
+
+        return maxSum;
+    }
+}
